a9688a9f4ee83dbb4056e3945e1b49548e88e805
Thiago Costa de Souza <thgsouza@outlook.com>
2022-12-21 18:24:08 -0300

    First commit

---
+++ src/main/java/br/com/souzathg/mongodbspringboot/MongodbSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.souzathg.mongodbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/souzathg/mongodbspringboot/MongodbSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.souzathg.mongodbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
