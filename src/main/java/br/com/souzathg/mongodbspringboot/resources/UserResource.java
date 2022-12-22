f8abf66b9197a3ebb127acffefa02a095cd8a764
Thiago Costa de Souza <thgsouza@outlook.com>
2022-12-21 23:01:48 -0300

    Implements /users POST

--- src/main/java/br/com/souzathg/mongodbspringboot/resources/UserResource.java
+++ src/main/java/br/com/souzathg/mongodbspringboot/resources/UserResource.java
@@ -1,13 +1,18 @@
 package br.com.souzathg.mongodbspringboot.resources;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.souzathg.mongodbspringboot.domain.User;
 import br.com.souzathg.mongodbspringboot.dto.UserDTO;
@@ -32,5 +37,14 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO userDTO) {
+        User user = service.fromDTO(userDTO);
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new UserDTO(user));
     }
 }
--- src/main/java/br/com/souzathg/mongodbspringboot/services/UserService.java
+++ src/main/java/br/com/souzathg/mongodbspringboot/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.ObjectError;
 
 import br.com.souzathg.mongodbspringboot.domain.User;
+import br.com.souzathg.mongodbspringboot.dto.UserDTO;
 import br.com.souzathg.mongodbspringboot.repositories.UserRepository;
 import br.com.souzathg.mongodbspringboot.services.exceptions.ObjectNotFoundException;
 
@@ -30,4 +31,13 @@ public class UserService {
             throw new ObjectNotFoundException(e.getMessage());
         }
     }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
+
 }
