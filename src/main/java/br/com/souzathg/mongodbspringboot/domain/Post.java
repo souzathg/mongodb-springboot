0ea1bdc480b7d1f38231c2f5e4e1b1befc73486e
Thiago Costa de Souza <thgsouza@outlook.com>
2022-12-22 11:47:31 -0300

    Implements AuthorDTO

--- src/main/java/br/com/souzathg/mongodbspringboot/domain/Post.java
+++ src/main/java/br/com/souzathg/mongodbspringboot/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import br.com.souzathg.mongodbspringboot.dto.AuthorDTO;
+
 @Document(collection = "post")
 public class Post implements Serializable {
 
@@ -14,11 +16,11 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {}
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
       this.id = id;
       this.date = date;
       this.title = title;
@@ -58,11 +60,11 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
